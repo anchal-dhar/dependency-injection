4c041a57f684794142327dd440677acdfa6807dd
Anchal Dhar <15966270+anchal-dhar@users.noreply.github.com>
2019-06-23 18:55:55 +0100

    Update build.sh

--- dagger/build.sh
+++ dagger/build.sh
@@ -1,12 +1,17 @@
--- Compiles the project
+# Compiles the project
 mvn compile
--- Creates the jar
+
+# Creates the jar
 mvn package
--- Creates the jar with all dependencies included
+
+# Creates the jar with all dependencies included
 mvn assembly:single
--- Puts all requiredjar on local maven install directry starting with .m2
+
+# Puts all required jar on local maven install directry starting with .m2
 mvn clean install
--- Run the main app using the mvn exec plugin
+
+# Run the main app using the mvn exec plugin
 mvn exec:java -Dexec.mainClass="coffee.CoffeeApp"
--- Run the dependencies jar , the normal jar cannot be run as it does not hold all dependencies
+
+# Run the dependencies jar , the normal jar cannot be run as it does not hold all dependencies
 java -jar target/coffee-shop-1.00-jar-with-dependencies.jar
